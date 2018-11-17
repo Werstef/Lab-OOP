921bab0ab2fc54edc9de0e269d8bb30455359da0
Werstef <32906385+Werstef@users.noreply.github.com>
2018-11-17 11:29:04 +0200

    change problem 5

--- Lab3-OOP-Teme/problem5.cpp
+++ Lab3-OOP-Teme/problem5.cpp
@@ -8,12 +8,13 @@ private:
     char* last_name;
     char* first_name;
     int group;
-    Curs* list_curses;
+    int* array_of_courses;
     Student* next;
 public:
+
     static int number_of_instances ;
     Student(){
-        grade = 10;
+
         number_of_instances++;
     }
     Student(int grade){
@@ -21,16 +22,16 @@ public:
         number_of_instances++;
     }
     ~Student(){
-       /* number_of_instances--; */
-        cout<< "Instance Destroyed" << endl;
+        number_of_instances--;
+        ///cout<< "Instance Destroyed" << endl;
     }
-    setGrade(int grade){
+    void setGrade(int grade){
         this->grade = grade;
     }
     int getGrade(){
         return this->grade;
     }
-    setNext(Student* next){
+    void setNext(Student* next){
         this->next = next;
     }
 
@@ -38,7 +39,7 @@ public:
         return this->next;
     }
 
-    setLastName(char* last_name){
+    void setLastName(char* last_name){
         this->last_name = last_name;
     }
 
@@ -46,7 +47,7 @@ public:
         return this->last_name;
     }
 
-    setFirstName(char* first_name){
+    void setFirstName(char* first_name){
         this->first_name = first_name;
     }
 
@@ -54,7 +55,7 @@ public:
         return this->first_name;
     }
 
-    setGroup(int group){
+    void setGroup(int group){
         this->group = group;
     }
 
@@ -62,21 +63,26 @@ public:
         return this->group;
     }
 
-    Curs* getListCurses(){
-        return this->list_curses;
+///    Curs* getListCourses(){
+        return this->list_courses;
+    }
+
+///    void addCursToList(Curs *curs){
+
     }
 
 };
 
 class Curs{
 private:
+    int cod_course;
     char* name;
     char* profesor;
     Student* list_members;
-    Curs* next
+    Curs* next;
 public:
 
-    setName(char* name){
+    void setName(char* name){
         this->name = name;
     }
 
@@ -84,7 +90,14 @@ public:
         return this->name;
     }
 
-    setProfesor(char* profesor){
+    bool compareName(char* name){
+        if(this->name == name){
+            return true;
+        }
+        return false;
+    }
+
+    void setProfesor(char* profesor){
         this->profesor = profesor;
     }
 
@@ -92,15 +105,27 @@ public:
         return this->profesor;
     }
 
-    Student* getListMembers(){
+    void setNext(Curs* next){
+        this->next = next;
+    }
+
+    Curs* getNext(){
+        return this->next;
+    }
+
+ ///   Student* getListMembers(){
         return this->list_members;
+    }
+
+ ///   void addStudentToList(Student *student){
+    ****
     }
 
 };
 
-void create_list_student(){
+Student* create_list_student(Curs* head_course){
 
-    cout<<"Type the number of instances you want to create" << endl;
+    cout<<"Type the number of students you want to add" << endl;
     int number_list_members;
     cin>> number_list_members;
 
@@ -127,27 +152,206 @@ void create_list_student(){
     head_student->setLastName(student_last_name);
     head_student->setFirstName(student_first_name);
     head_student->setGroup(student_group);
+
+    cout<<"How many courses does the student go to?" << endl;
+    int number_of_courses;
+    cin >> number_of_courses;
+    for(int iterator = 0; iterator < number_of_courses; iterator ++){
+        cout << "Write the course that the student goes to" << endl;
+        char* course;
+        cin >> course;
+        bool course_attended;
+        Curs* iterator_list_courses;
+        iterator_list_courses = head_list_courses;
+        for(int iterator = 0; iterator < existing_courses; iterator++){
+            if(iterator_list_courses->compareName(course)){
+                head_student->
+                head_student->addCursToList(iterator_list_courses);
+                iterator_list_courses = head_course;
+                break;
+            }
+            iterator_list_courses = iterator_list_courses->getNext();
+        }
+
+    }
+
+
+
     iterator_list = head_student;
-//continuare functie
     for(int iterator = 0; iterator < number_list_members - 1; iterator++){
 
         cout<<"Write the grade for the student" << endl;
         int grade_student;
         cin >> grade_student;
 
+        cout<<"Write the Last name of the student" << endl;
+        char* student_first_name;
+        cin >> student_first_name;
+
+        cout<<"Write the First Name of the student" << endl;
+        char* student_last_name;
+        cin >> student_last_name;
+
+        cout<<"Write the Group of the student" << endl;
+        int student_group;
+        cin >> student_group;
+
 
         Student* new_student = new Student(grade_student);
 
-        if(iterator == 0){
-            head_student->setNext(new_student);
-        }
-        else{
-            iterator_list->setNext(new_student);
-
-        }
+        iterator_list->setNext(new_student);
 
         new_student->setNext(NULL);
+        new_student->setLastName(student_last_name);
+        new_student->setFirstName(student_first_name);
+        new_student->setGroup(student_group);
+
+        cout<<"How many courses does the student go to?" << endl;
+        int number_of_courses;
+        cin >> number_of_courses;
+        for(int iterator = 0; iterator < number_of_courses; iterator ++){
+            cout << "Write the course that the student goes to" << endl;
+            char* course;
+            cin >> course;
+            bool course_attended;
+            Curs* iterator_list_courses;
+            iterator_list_courses = head_list_courses;
+            for(int iterator = 0; iterator < existing_courses; iterator++){
+                if(iterator_list_courses->compareName(course)){
+                    iterator_list_courses->addStudentToList(new_student);
+                    new_student->addCursToList(iterator_list_courses);
+                    iterator_list_courses = head_course;
+                    break;
+                }
+                iterator_list_courses = iterator_list_courses->getNext();
+            }
+
+        }
+
         iterator_list = new_student;
+    }
+
+    return head_student;
+}
+
+void delete_student(char* last_name, Student* head_student){
+
+    Student* iterator_list_student = new Student();
+    Student* previous_list_student = new Student();
+    previous_list_student = head_student;
+    iterator_list_student = head_student;
+
+    while(iterator_list_student != NULL){
+        if(iterator_list_student->getLastName() == last_name){
+                if(iterator_list_student == head_student){
+                    head_student = iterator_list_student->getNext();
+                    delete(iterator_list_student);
+                }
+                else{
+                    previous_list_student->setNext(iterator_list_student->getNext());
+                    iterator_list_student->setNext(NULL);
+                    free(iterator_list_student);
+                }
+        if(iterator_list_student != head_student){
+            previous_list_student = previous_list_student->getNext();
+        }
+
+        }
+    }
+    delete(previous_list_student);
+
+}
+
+void edit_student(char* last_name, Student* head_student){
+
+    Student* iterator_list_student = new Student();
+    iterator_list_student = head_student;
+
+    while(iterator_list_student != NULL){
+        if(iterator_list_student->getLastName() == last_name){
+            cout<<"Write the new Last Name of the student" << endl;
+            char* last_name_modified;
+            cin >> last_name_modified;
+
+            cout<<"Write the new First Name of the student" << endl;
+            char* first_name_modified;
+            cin >> first_name_modified;
+
+            cout<<"Write the new Group of the student" << endl;
+            int group_modified;
+            cin >> group_modified;
+
+            cout<<"Write the new Grade of the student" << endl;
+            int grade_modified;
+            cin >> grade_modified;
+
+            iterator_list_student->setFirstName(first_name_modified);
+            iterator_list_student->setLastName(last_name_modified);
+            iterator_list_student->setGroup(group_modified);
+            iterator_list_student->setGrade(grade_modified);
+            return;
+        }
+        iterator_list_student = iterator_list_student->getNext();
+    }
+}
+
+Curs* crerate_list_courses(){
+
+    cout << "Write the number of courses for this year" << endl;
+    int number_of_courses;
+    cin >> number_of_courses;
+
+    Curs* head_course = new Curs();
+    head_course->setNext(NULL);
+
+    cout<< "Write the name of the course" << endl;
+    char* course_name;
+    cin >> course_name;
+
+    cout << "Write the name of the teacher" << endl;
+    char* prof_name;
+    cin >> prof_name;
+
+    head_course->setName(course_name);
+    head_course->setProfesor(prof_name);
+
+    Curs* iterator_courses = head_course;
+
+    for(int iterator = 0; iterator < number_of_courses - 1; iterator++){
+        Curs* new_course = new Curs();
+        new_course->setNext(NULL);
+        iterator_courses->setNext(new_course);
+
+        cout<< "Write the name of the course" << endl;
+        char* course_name;
+        cin >> course_name;
+
+        cout << "Write the name of the teacher" << endl;
+        char* prof_name;
+        cin >> prof_name;
+
+        new_course->setName(course_name);
+        new_course->setProfesor(prof_name);
+
+        iterator_courses = iterator_courses->getNext();
+
+    }
+
+    return head_course;
+
+}
+
+Student* look_for_student(char* last_name, Student* head_student){
+
+    Student* iterator_list_student = new Student();
+
+    iterator_list_student = head_student;
+
+    while(iterator_list_student != NULL){
+        if(iterator_list_student->getLastName() == last_name){
+            return iterator_list_student;
+        }
+        iterator_list_student = iterator_list_student->getNext();
     }
 
 }
@@ -179,47 +383,40 @@ void display_list_student(Student* head_student, int number_list_members){
     }
 }
 
+
 int Student::number_of_instances = 0;
 
 int main () {
 
+    Curs *head_course;
+    Curs *head_student;
+    /// 1)
+    head_course = crerate_list_courses();
+    head_student = create_list_student(head_course);
 
-    cout<<"Type the number of instances you want to create" << endl;
-    int number_list_members;
-    cin>> number_list_members;
-
-    Student* iterator_list;
-
-    cout<<"Write the grade for the student" << endl;
-    int grade_student;
-    cin >> grade_student;
-
-    Student* head_student = new Student(grade_student);
-    head_student->setNext(NULL);
-    iterator_list = head_student;
-
-    for(int iterator = 0; iterator < number_list_members - 1; iterator++){
-
-        cout<<"Write the grade for the student" << endl;
-        int grade_student;
-        cin >> grade_student;
+    /// 2)
+    cout<< "Write the last name of the student you want to delete" << endl;
+    char* student_last_name;
+    cin >> student_last_name;
+    delete_student(student_last_name, head_student);
 
 
-        Student* new_student = new Student(grade_student);
+    cout<< "Write the last name of the student you want to edit" << endl;
+    cin >> student_last_name;
+    edit_student(student_last_name, head_student);
 
-        if(iterator == 0){
-            head_student->setNext(new_student);
-        }
-        else{
-            iterator_list->setNext(new_student);
+    /// 3)
+    cout<< "The number of students is " << number_of_instances;
 
-        }
+    /// 4)
+    cout<< "Write the last name of the student you want to find" << endl;
+    cin >> student_last_name;
+    Student* found_student = new Student*;
+    found_student = look_for_student(student_last_name, head_student);
 
-        new_student->setNext(NULL);
-        iterator_list = new_student;
-    }
+    /// 5)
 
-    iterator_list->setNext(head_student);
+
     display_list(head_student, number_list_members);
 
     return 0;
