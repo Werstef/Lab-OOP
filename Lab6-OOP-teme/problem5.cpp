abbed39cf35677c9545ec2466d716e127605c1de
Werstef <32906385+Werstef@users.noreply.github.com>
2018-12-03 14:09:13 +0200

    finished lab 6

--- Lab6-OOP-teme/problem5.cpp
+++ Lab6-OOP-teme/problem5.cpp
@@ -3,55 +3,44 @@
 
 using namespace std;
 
-class NrComplex {
-private:
-    float re;
-    float im;
+class NrComp {
 public:
-
-    NrComplex(float re, float im){
-        this->re = re;
-        this->im = im;
-    }
-
-    void setIm(float im){
-        this->im = im;
-    }
-    float getIm(){
-        return this->im;
-    }
-
-    void setRe(float re){
-        this->re = re;
-    }
-
-    float getRe(){
-        return this->re;
-    }
-
-    double operator~(NrComplex parametru){
-        return sqrt(parametru.getRe() * parametru.getRe() + parametru.getIm() * parametru.getIm());
-    }
-
-    void operator^(NrComplex parametru){
-        parametru.setRe(parametru.getRe() * parametru.getRe());
-        parametru.setIm(parametru.getIm() * parametru.getIm());
-    }
-
+	double re;
+	double im;
+	NrComp(double re, double im) {
+		this->re= re;
+		this->im = im;
+	}
+	friend double operator~(const NrComp &numar);
+	friend NrComp operator^(const NrComp &numar, const int power);
 };
 
+double operator~(const NrComp &numar) {
+	return sqrt(numar.re * numar.im + numar.re * numar.im);
+}
 
+NrComp operator^(const NrComp &numar, const int power) {
+	NrComp numar_aux = numar;
+	double re_aux;
+	double im_aux;
+	for (int i = 1; i < power; i++) {
+		re_aux = numar_aux.re * numar.re - numar_aux.im * numar.im;
+		im_aux = numar_aux.re * numar.im + numar_aux.im * numar.re;
+
+		numar_aux.re = re_aux;
+		numar_aux.im = im_aux;
+	}
+	return numar_aux;
+}
 
 int main () {
+	NrComp numar_c(3.0, 4.0);
 
-    double x = 3;
-    double y = 2;
+	cout << "Modulus of our number is " << ~numar_c << endl;
 
-    cout << x^2 << endl;
+	numar_c = numar_c^2;
 
-    NrComplex nr1(2, 3);
-
-    cout << nr1~ << endl;
+	cout << "The number at the power of 2 is " <<numar_c.re << " + " << numar_c.im << "i" << endl;
 
     return 0;
 }
