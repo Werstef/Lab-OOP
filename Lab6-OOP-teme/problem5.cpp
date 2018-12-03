4aeff3852c91269143808e7cb69794285c476397
Werstef <32906385+Werstef@users.noreply.github.com>
2018-12-03 14:16:00 +0200

    finished lab 6

--- Lab6-OOP-teme/problem5.cpp
+++ Lab6-OOP-teme/problem5.cpp
@@ -4,43 +4,57 @@
 using namespace std;
 
 class NrComp {
-public:
-	double re;
+protected:
+    double re;
 	double im;
+public:
 	NrComp(double re, double im) {
 		this->re= re;
 		this->im = im;
 	}
+	void setRe(double re){
+        this->re = re;
+	}
+	double getRe(){
+        return this->re;
+	}
+    void setIm(double im){
+        this->im = im;
+	}
+	double getIm(){
+        return this->im;
+	}
+
 	friend double operator~(const NrComp &numar);
 	friend NrComp operator^(const NrComp &numar, const int power);
 };
 
-double operator~(const NrComp &numar) {
-	return sqrt(numar.re * numar.im + numar.re * numar.im);
+double operator~(NrComp &numar) {
+	return sqrt(numar.getRe() * numar.getIm() + numar.getRe() * numar.getIm());
 }
 
-NrComp operator^(const NrComp &numar, const int power) {
+NrComp operator^(NrComp &numar, const int power) {
 	NrComp numar_aux = numar;
 	double re_aux;
 	double im_aux;
 	for (int i = 1; i < power; i++) {
-		re_aux = numar_aux.re * numar.re - numar_aux.im * numar.im;
-		im_aux = numar_aux.re * numar.im + numar_aux.im * numar.re;
+		re_aux = numar_aux.getRe() * numar.getRe() - numar_aux.getIm() * numar.getIm();
+		im_aux = numar_aux.getRe() * numar.getIm() + numar_aux.getIm() * numar.getRe();
 
-		numar_aux.re = re_aux;
-		numar_aux.im = im_aux;
+		numar_aux.setRe(re_aux);
+		numar_aux.setIm(im_aux);
 	}
 	return numar_aux;
 }
 
 int main () {
-	NrComp numar_c(3.0, 4.0);
+	NrComp numar(3.0, 4.0);
 
-	cout << "Modulus of our number is " << ~numar_c << endl;
+	cout << "Modulus of our number is " << ~numar << endl;
 
-	numar_c = numar_c^2;
+	numar = numar^2;
 
-	cout << "The number at the power of 2 is " <<numar_c.re << " + " << numar_c.im << "i" << endl;
+	cout << "The number at the power of 2 is " <<numar.getRe() << " + " << numar.getIm() << "i" << endl;
 
     return 0;
 }
